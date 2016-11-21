321434095f839df091e9610ad75a4465a6aeb2df
KristinaBarutska <kristina.barutska@abv.bg>
2016-11-21 20:43:55 +0200

    test

--- FindYourTransport.Api/app.js
+++ FindYourTransport.Api/app.js
@@ -6,6 +6,6 @@ const config = require("./server/configurations");
 const app = express();
 
 require("./server/configurations/database")(config);
-
+app.set("views", config.view);
 app.listen(config.port);
 console.log(`Server listens on ${config.port}`);
