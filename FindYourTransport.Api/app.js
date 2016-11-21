174e998699a498df8a7705b1290b70f265f78980
Tsvetoslav Lazarov <nearfs@abv.bg>
2016-11-21 17:07:04 +0200

    Added configurations for express and model and basic user model (to be updated later). Working on registration

--- FindYourTransport.Api/app.js
+++ FindYourTransport.Api/app.js
@@ -6,6 +6,8 @@ const config = require("./server/configurations");
 const app = express();
 
 require("./server/configurations/database")(config);
+require("./server/configurations/express")(app, config);
+require("./server/configurations/passport")();
 
 app.listen(config.port);
 console.log(`Server listens on ${config.port}`);
