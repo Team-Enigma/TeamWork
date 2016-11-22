388c8608f580d68f12b4baa76096bce1a801d063
Tone <tone_92@abv.bg>
2016-11-22 22:53:22 +0200

    I believe I fixed my bullsh*t. Now it's working and it has "/home" in homepage url.

--- FindYourTransport.Api/server/controllers/home-controller.js
+++ FindYourTransport.Api/server/controllers/home-controller.js
@@ -1,33 +1,9 @@
-<<<<<<< HEAD
-var pug = require("pug");
-
-function getHomePageView(req, res) {
-    var nav, home;
-
-    // if user logged in show logged in navigation 
-    // home = pug.compileFile("home-logged-in.pug");
-    // nav = pug.compileFile("nav-logged-in.pug");
-
-    // else
-    home = pug.compileFile("server/views/home-not-logged-in.pug");
-    nav = pug.compileFile("server/views/nav-not-logged-in.pug");
-
-    res.send(home() + nav());
-}
+function loadHomePage(req, res) {
+    res.render("home-views/home", { user: req.user });
+};
 
 function redirectToHome(req, res) {
     res.redirect("/home");
 }
 
-
-module.exports = {
-    getHomePageView,
-    redirectToHome
-}
-=======
-function loadHomePage(req, res) {
-    res.render("home-views/home", { user: req.user });
-};
-
-module.exports = { loadHomePage };
->>>>>>> 0a4828da1dd5d4140061067be7d69174a523a480
+module.exports = { loadHomePage, redirectToHome };
