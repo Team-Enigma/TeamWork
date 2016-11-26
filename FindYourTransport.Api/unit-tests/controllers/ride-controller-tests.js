b900cb2828c7ed389d8adc9f5cc6e0b61f0fe1dc
todor-enikov <enigmaenikov@gmail.com>
2016-11-26 13:10:34 +0200

    Fixed tests in ride-controller-tests and added tests for home-controller-tests.

--- FindYourTransport.Api/unit-tests/controllers/ride-controller-tests.js
+++ FindYourTransport.Api/unit-tests/controllers/ride-controller-tests.js
@@ -4,33 +4,33 @@ var rideController = require("../../server/controllers/ride-controller");
 //var mockery = require("mockery");
 var sinon = require("sinon");
 
-describe('User-controller.js testing', function() {
+describe('Ride-controller.js testing', function() {
 
-    it('loadFilteredUsers should be a function', function() {
+    it('addNewRide should be a function', function() {
         expect(rideController.addNewRide).to.be.a('function');
     });
 
-    it('loadLoginPage should be a function', function() {
+    it('addPassenger should be a function', function() {
         expect(rideController.addPassenger).to.be.a('function');
     });
 
-    it('loadRegisterPage should be a function', function() {
+    it('calculatePrice should be a function', function() {
         expect(rideController.calculatePrice).to.be.a('function');
     });
 
-    it('loadUserByUserName should be a function', function() {
+    it('loadAllRides should be a function', function() {
         expect(rideController.loadAllRides).to.be.a('function');
     });
 
-    it('loadUsers should be a function', function() {
+    it('loadFilteredRides should be a function', function() {
         expect(rideController.loadFilteredRides).to.be.a('function');
     });
 
-    it('loginUser should be a function', function() {
+    it('loadNewRidePage should be a function', function() {
         expect(rideController.loadNewRidePage).to.be.a('function');
     });
 
-    it('logoutUser should be a function', function() {
+    it('loadSpecificRide should be a function', function() {
         expect(rideController.loadSpecificRide).to.be.a('function');
     });
 });
