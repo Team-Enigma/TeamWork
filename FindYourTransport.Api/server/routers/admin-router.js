efef678b0068031e22d5b8453f44f9019255271a
Tsvetoslav Lazarov <nearfs@abv.bg>
2016-12-01 20:26:13 +0200

    Added authentication for admin routes and some minor changes in views in admin page and contact us page

--- FindYourTransport.Api/server/routers/admin-router.js
+++ FindYourTransport.Api/server/routers/admin-router.js
@@ -1,5 +1,5 @@
 module.exports = function(app, authenticator, validator, controllers) {
-    app.get("/admin", controllers.admin.loadAdminPage);
-    app.get("/admin/messages/:id", controllers.admin.loadAdminDetailedMessage);
-    app.post("/admin/messages", controllers.admin.updateMessageStatus);
+    app.get("/admin", authenticator.authenticateRole("Admin"), controllers.admin.loadAdminPage);
+    app.get("/admin/messages/:id", authenticator.authenticateRole("Admin"), controllers.admin.loadAdminDetailedMessage);
+    app.post("/admin/messages", authenticator.authenticateRole("Admin"), controllers.admin.updateMessageStatus);
 };
