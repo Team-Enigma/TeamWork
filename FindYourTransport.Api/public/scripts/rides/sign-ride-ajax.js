f27d10ae60ff1ed08b000600b0bd9b57c8190992
Tsvetoslav Lazarov <nearfs@abv.bg>
2016-12-03 12:12:10 +0200

    Client-side for sign for ride done (eventually add sign out from ride when all client-side is done)

---
+++ FindYourTransport.Api/public/scripts/rides/sign-ride-ajax.js
@@ -0,0 +1,28 @@
+var app = app || {};
+
+$("#tb-sign-for-ride").on("click", (ev) => {
+    const rideId = $("#tb-ride-id").val();
+    const passengerUsername = $("#tb-ride-passenger").val();
+
+    let data = {
+        rideId,
+        passengerUsername
+    };
+
+    app.requester.put("/rides/sign", data)
+        .then(response => {
+            let parsedResponse = JSON.parse(response);
+            if (parsedResponse.success) {
+                app.notificator.showNotification(parsedResponse.success, "success");
+                setTimeout(() => {
+                    window.location.href = `/rides/${rideId}`;
+                }, 1000);
+            } else if (parsedResponse.error) {
+                app.notificator.showNotification(parsedResponse.error, "error");
+            }
+        })
+        .catch(err => {
+            let parsedError = JSON.parse(err.responseJSON);
+            app.notificator.showNotification(parsedError.error, "error");
+        });
+});
--- FindYourTransport.Api/server/routers/ride-router.js
+++ FindYourTransport.Api/server/routers/ride-router.js
@@ -2,11 +2,10 @@ const queryStringBuilder = require("../utils/query-string-builder");
 
 module.exports = function(app, authenticator, validator, controllers) {
     app.get("/rides", controllers.ride.loadFilteredRides);
+    app.put("/rides/sign", authenticator.authenticateLoggedUserPostRequests, controllers.ride.addPassenger);
+    app.get("/rides/add", authenticator.authenticateLoggedUser, controllers.ride.loadNewRidePage);
+    app.post("/rides/add", authenticator.authenticateLoggedUserPostRequests, validator.validateRideCreation, controllers.ride.addNewRide);
     app.get("/rides/:id", authenticator.authenticateLoggedUser, controllers.ride.loadSpecificRide);
     app.post("/rides/:id", authenticator.authenticateLoggedUserPostRequests, controllers.ride.removeRideById);
 
-    app.post("/sign-for-ride", authenticator.authenticateLoggedUserPostRequests, controllers.ride.addPassenger);
-
-    app.get("/add-ride", authenticator.authenticateLoggedUser, controllers.ride.loadNewRidePage);
-    app.post("/add-ride", authenticator.authenticateLoggedUserPostRequests, validator.validateRideCreation, controllers.ride.addNewRide);
 };
